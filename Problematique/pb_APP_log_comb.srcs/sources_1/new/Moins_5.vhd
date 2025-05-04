74be4da2c7ca3412392393feea5107589a24cf69
Franky55 <43887787+Franky55@users.noreply.github.com>
2025-05-04 16:54:50 -0400

    Bin2DualBCD_NS_tb, done

---
+++ Problematique/pb_APP_log_comb.srcs/sources_1/new/Moins_5.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/04/2025 04:18:00 PM
+-- Design Name: 
+-- Module Name: Moins_5 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Moins_5 is
+    Port ( ADCbin : in STD_LOGIC_VECTOR (3 downto 0);
+           Moins5 : out STD_LOGIC_VECTOR (3 downto 0));
+end Moins_5;
+
+architecture Behavioral of Moins_5 is
+
+begin
+
+
+end Behavioral;
