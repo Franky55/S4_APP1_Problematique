ea2a67e7ace5aa63b5bd088284760aaf77f63fff
Franky55 <43887787+Franky55@users.noreply.github.com>
2025-05-05 19:26:39 -0400

    Sauvegarde forgot

--- Problematique/pb_APP_log_comb.srcs/sources_1/new/Thermo2Bin_4Bits.vhd
+++ Problematique/pb_APP_log_comb.srcs/sources_1/new/Thermo2Bin_4Bits.vhd
@@ -38,8 +38,25 @@ entity Thermo2Bin_4Bits is
 end Thermo2Bin_4Bits;
 
 architecture Behavioral of Thermo2Bin_4Bits is
-
+    
+    signal A : std_logic;
+    signal B : std_logic;
+    signal C : std_logic;
+    signal D : std_logic;
+    
 begin
 
+    A <= ADC4Bits(3);
+    B <= ADC4Bits(2);
+    C <= ADC4Bits(1);
+    D <= ADC4Bits(0);
+    
+    erreur <= (A AND NOT B) OR (C AND NOT D) OR (B AND NOT C);
 
+    ADCOut4Bits(0) <= (NOT A AND B) OR (NOT C AND D);
+    ADCOut4Bits(1) <= (NOT A AND C);
+    ADCOut4Bits(2) <= A;
+    ADCOut4Bits(3) <= '0'; 
+    
+    
 end Behavioral;
